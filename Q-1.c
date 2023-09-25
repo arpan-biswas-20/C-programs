b0fe478fd32879c471773b54f36b5b4740028267
Arpan Biswas <106981410+arpan-biswas-20@users.noreply.github.com>
2023-09-25 23:43:36 +0530

    Add files via upload

---
+++ Q-1.c
@@ -0,0 +1,11 @@
+// Q-1. Sum of two numbers given by users.
+#include<stdio.h>
+int main()
+{
+    int num1,num2,sum;
+    printf("Enter two numbers : ");
+    scanf("%d%d"),&num1,&num2;
+
+    sum=num1+num2;
+    printf("%d+%d=%d\n",num1,num2,sum);
+}
